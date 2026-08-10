0d2c9aae2ae853ef907d05d58da1161f103ca3f7
mothagg <lino.packygg@gmail.com>
2026-08-10 04:49:11 +0200

    Align account review guide loading state

--- src/app/(antifraud)/antifraud/guide/account-review/loading.tsx
+++ src/app/(antifraud)/antifraud/guide/account-review/loading.tsx
@@ -1,5 +1,5 @@
 import { GuideLoading } from "../_components/guide-primitives";
 
 export default function AntifraudAccountReviewGuideLoading() {
-  return <GuideLoading panels={7} />;
+  return <GuideLoading panels={8} />;
 }
